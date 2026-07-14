e7291d1b5a3f68981fb4ca3322221b292ae8479a
125hz <nickfugitives@gmail.com>
2026-07-13 22:50:25 -0500

    Fix VitaSDK fixture link

--- ios/fixtures/vitasdk-milestone10/main.c
+++ ios/fixtures/vitasdk-milestone10/main.c
@@ -1,13 +1,11 @@
 #include <psp2/kernel/threadmgr/thread.h>
 #include <psp2/types.h>
 
-void _start(void) __attribute__((weak, alias("module_start")));
+int _start(SceSize args, void *argp) __attribute__((weak, alias("module_start")));
 
 int module_start(SceSize args, void *argp) {
     (void)args;
     (void)argp;
 
-    const int thread_id = sceKernelGetThreadId();
-    sceKernelExitThread(thread_id & 0xFF);
-    return 0;
+    return sceKernelGetThreadId() & 0xFF;
 }
